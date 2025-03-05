c51ff6ba6b3eceddea4259115721d2b5ed45b491
Yoann Leblanc <yoannleblanc88@gmail.com>
2025-03-05 22:17:00 +0100

    update yelp mvp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,10 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.save
